bc4b4414f828a517618b4ef0dd96d238bd1db160
fufu7755 <fufu7755@gmail.com>
2016-09-22 16:51:57 +0800

    test

--- public/app/app.js
+++ public/app/app.js
@@ -1,25 +1,26 @@
 'use strict';
 
 angular.module('hshs', [
-  'ngRoute',
-  'ngSanitize',
-  'ngStorage',
-  'toaster',
-  'angularUtils.directives.dirPagination',
-  'ui.mask'
+    'ngRoute',
+    'ngSanitize',
+    'ngStorage',
+    'toaster',
+    'angularUtils.directives.dirPagination',
+    'ui.mask'
 ]).
 constant(
-  'baseUrl', 'http://data-soul.nodefu.net/api/'
+    'baseUrl', 'http://data-soul.nodefu.net/api/'
 ).
-config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-      .when('/', { templateUrl: 'views/pages/home.html' })
-      .when('/cases', { templateUrl: 'views/pages/cases.html' })
-      .when('/cases/:caseId', { templateUrl: 'views/pages/case.html' })
-        .when('/service', { templateUrl: 'views/pages/service.html' })
+        .when('/', {templateUrl: 'views/pages/home.html'})
+        .when('/cases', {templateUrl: 'views/pages/cases.html'})
+        .when('/cases/:caseId', {templateUrl: 'views/pages/case.html'})
+        .when('/service', {templateUrl: 'views/pages/service.html'})
+        .when('/culture', {templateUrl: 'views/pages/culture.html'})
 
-        .otherwise({ redirectTo: '/' });
+        .otherwise({redirectTo: '/'});
 }]).run([
     '$rootScope',
     '$location',
@@ -27,14 +28,18 @@ config(['$routeProvider', '$locationProvider', function($routeProvider, $locatio
     function ($rootScope, $location) {
         var currentRoute = $location.path().split('/');
 
-        if($location.path() == '/') {
+        if ($location.path() == '/') {
             $rootScope.bodyClass = 'homePage';
         }
-        if(currentRoute[1] == 'cases' && currentRoute[2]) {
+        if (currentRoute[1] == 'cases' && currentRoute[2]) {
             $rootScope.bodyClass = 'transparent caseShow';
         }
 
-        if(currentRoute[1] == 'service') {
+        if (currentRoute[1] == 'service') {
+            $rootScope.bodyClass = 'transparent';
+        }
+
+        if (currentRoute[1] == 'culture') {
             $rootScope.bodyClass = 'transparent';
         }
     }
